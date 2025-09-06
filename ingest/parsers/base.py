20cc2342d9055d30fbee2e56345f712a54403071
artem <miroshnichenkoartem0307@gmail.com>
2025-09-06 11:46:44 +0300

    added fetch_soup method for BaseHTTPParser

--- ingest/parsers/base.py
+++ ingest/parsers/base.py
@@ -1,18 +1,17 @@
 from __future__ import annotations
 from typing import Optional, Callable
 import requests
+from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
 
 class BaseHTTPParser:
     def __init__(
         self,
-        base_url: str,
         fetch_func: Optional[Callable[[str], str]] = None,
         timeout: int = 15,
         user_agent: Optional[str] = None,
     ):
-        self.base_url = base_url.rstrip('/') + '/'
         self._fetch_func = fetch_func
         self._timeout = timeout
 
@@ -26,3 +25,7 @@ class BaseHTTPParser:
         resp = requests.get(url, headers=headers, timeout=self._timeout)
         resp.raise_for_status()
         return resp.text
+
+    def fetch_soup(self, url: str) -> BeautifulSoup:
+        html = self.fetch(url=url)
+        return BeautifulSoup(html, 'html.parser')
