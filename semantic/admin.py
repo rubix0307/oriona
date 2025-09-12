4f803df85a3f57bbc8beebe82f461edddd8ba3d9
artem <miroshnichenkoartem0307@gmail.com>
2025-09-13 00:42:54 +0300

    added readonly_fields for IngestArticleEmbeddingAdmin+

--- semantic/admin.py
+++ semantic/admin.py
@@ -3,7 +3,8 @@ from .models import IngestArticleEmbedding
 
 
 @admin.register(IngestArticleEmbedding)
-class ArticleAdmin(admin.ModelAdmin):
+class IngestArticleEmbeddingAdmin(admin.ModelAdmin):
     list_display = (
         'article', 'MODEL',
     )
+    readonly_fields = ('article',)
