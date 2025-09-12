df345ea0649fecbb69765d44ac84a62ac9a17a22
artem <miroshnichenkoartem0307@gmail.com>
2025-09-12 22:54:00 +0300

    change parse articles with chunks

--- ingest/management/commands/collect_articles.py
+++ ingest/management/commands/collect_articles.py
@@ -1,5 +1,7 @@
-
+from itertools import batched
 from django.core.management.base import BaseCommand
+
+from ingest.models import Article
 from ingest.parsers.factroom.parser import FactroomParser
 from ingest.services.article_persist import ArticlePersistService
 from ingest.services.feedcard_persist import FeedCardPersistService
@@ -8,10 +10,19 @@ from ingest.services.feedcard_persist import FeedCardPersistService
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
-        parser = FactroomParser()
-        cards = parser.parse_feeds(parse_depth=1)
-        articles = parser.parse_articles(cards=cards)
 
-        cards_stats = FeedCardPersistService().save_many(site=parser.site, cards=cards)
-        articles_stats = ArticlePersistService().save_many(site=parser.site, items=articles)
+        parser = FactroomParser()
+
+        parse_cards = False
+        if parse_cards:
+            cards = parser.parse_feeds(parse_depth=1)
+            cards_stats = FeedCardPersistService().save_many(site=parser.site, cards=cards)
+        else:
+            cards = Article.objects.filter(site=parser.site, content__content_text__isnull=True)
+
+
+        for chunk in batched(cards, 10):
+            articles = parser.parse_articles(cards=chunk)
+            articles_stats = ArticlePersistService().save_many(site=parser.site, items=articles)
+
         pass
