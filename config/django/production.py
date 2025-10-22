c0c68410787c89f4f3ba88d15fc4a3d96591fa40
Artem Miroshnychenko <miroshnichenkoartem0307@gmail.com>
2025-10-22 23:48:34 +0300

    change checking required variables

--- config/django/production.py
+++ config/django/production.py
@@ -8,10 +8,14 @@ CSRF_TRUSTED_ORIGINS = [
     f'https://{HOST}',
 ]
 BASE_URL: str = f'https://{HOST}'
-if not HOST:
-    raise ValueError('Please set the HOST environment variable')
 AGENTS_BASE_URL = os.getenv('AGENTS_BASE_URL')
 AGENTS_TOKEN = os.getenv('AGENTS_TOKEN')
+
+
+required_vars = ['HOST', 'AGENTS_BASE_URL', 'AGENTS_TOKEN']
+missing = [var for var in required_vars if not os.getenv(var)]
+if missing:
+    raise ValueError(f"Missing required environment variables: {', '.join(missing)}")
 
 from config.settings.sentry import *
 
